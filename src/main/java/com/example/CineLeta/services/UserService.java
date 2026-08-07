a7dc6d29dbb0c001091f01c45f9f71502e38ebfb
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-08-07 20:51:44 -0300

    feat: implementacao do UserService

---
+++ src/main/java/com/example/CineLeta/services/UserService.java
@@ -0,0 +1,34 @@
+package com.example.CineLeta.services;
+
+import com.example.CineLeta.models.User;
+import com.example.CineLeta.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User processOAuthPostLogin(String email, String googleId, String name){
+        Optional<User> usuario = userRepository.findByGoogleId(googleId);
+
+        if(usuario.isPresent()){
+            return usuario.get();
+        }
+
+        User user = new User();
+        user.setEmail(email);
+        user.setGoogleId(googleId);
+        user.setNickname(name);
+
+        userRepository.save(user);
+
+        return user;
+    }
+
+}
