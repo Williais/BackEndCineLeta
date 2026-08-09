3a2ac673fc31632159d1165c0b76022152e0386c
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-08-09 17:35:52 -0300

    feat(auth): implementa fluxo completo de login via Google OAuth2 e persistencia de usuario

---
+++ src/main/java/com/example/CineLeta/config/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,37 @@
+package com.example.CineLeta.config;
+
+import com.example.CineLeta.models.User;
+import com.example.CineLeta.services.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final UserService userService;
+
+    public OAuth2LoginSuccessHandler(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+        String userId = oAuth2User.getAttribute("sub");
+
+        User usuarioLogado = userService.processOAuthPostLogin(email, userId, name);
+
+        response.sendRedirect("/api/oscar/random");
+    }
+}
--- src/main/java/com/example/CineLeta/config/SecurityConfig.java
+++ src/main/java/com/example/CineLeta/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.CineLeta.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,16 +10,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    public SecurityConfig(OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler) {
+        this.oAuth2LoginSuccessHandler = oAuth2LoginSuccessHandler;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain( HttpSecurity http) throws Exception{
-        SecurityFilterChain config = http.csrf(csrf -> csrf.disable())
+        SecurityFilterChain configs = http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/oscar/**")
                         .permitAll()
                         .requestMatchers("/error")
                         .permitAll()
                         .anyRequest()
-                        .authenticated()).build();
+                        .authenticated())
+                .oauth2Login(oauth2 -> oauth2.successHandler(oAuth2LoginSuccessHandler))
+                .build();
 
-        return config;
+        return configs;
     }
 }
